52c6237073d3df3aa2cfdc67b30dd3e9a358ceac
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2024-01-18 10:28:36 +0700

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import EmailIcon from '@mui/icons-material/Email';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TelegramIcon from '@mui/icons-material/Telegram';
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div>
+        Footer
+        <FacebookIcon />
+        <YouTubeIcon />
+        <EmailIcon />
+        <TwitterIcon />
+        <InstagramIcon />
+        <TelegramIcon />
+    </div>
+  )
+}
+
+export default Footer
